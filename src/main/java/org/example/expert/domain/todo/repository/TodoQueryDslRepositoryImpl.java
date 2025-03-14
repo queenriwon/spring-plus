666c2487c946d5eb834e6e56c8c3d2ce361d7778
queenriwon <queenriwon3@gmail.com>
2025-03-15 03:50:26 +0900

    feat: lv.3-1

--- src/main/java/org/example/expert/domain/todo/repository/TodoQueryDslRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryDslRepositoryImpl.java
@@ -1,11 +1,22 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.dto.response.TodoGetResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static org.example.expert.domain.comment.entity.QComment.comment;
+import static org.example.expert.domain.manager.entity.QManager.manager;
 import static org.example.expert.domain.todo.entity.QTodo.todo;
 
 @RequiredArgsConstructor
@@ -21,5 +32,56 @@ public class TodoQueryDslRepositoryImpl implements TodoQueryDslRepository{
                         .where(todo.id.eq(todoId))
                         .fetchFirst()
         );
+    }
+
+    @Override
+    public Page<TodoGetResponse> findByTitleOrCreatedAtOrManager(
+            String title,
+            LocalDate startAt,
+            LocalDate endAt,
+            String managerName,
+            Pageable pageable) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (title != null) {
+            builder.and(todo.title.contains(title));
+        }
+
+        if (startAt != null) {
+            builder.and(todo.createdAt.goe(startAt.atStartOfDay()));
+        }
+
+        if (endAt != null) {
+            builder.and(todo.createdAt.loe(endAt.atStartOfDay()));
+        }
+
+        if (managerName != null) {
+            builder.and(manager.user.nickname.contains(managerName));
+        }
+
+        List<TodoGetResponse> todoGetResponseList = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                TodoGetResponse.class,
+                                todo.title,
+                                manager.countDistinct(),
+                                comment.countDistinct()
+                        )
+                )
+                .from(todo)
+                .leftJoin(manager).on(manager.todo.id.eq(todo.id))      // fetchJoin(): 프로젝션 x
+                .leftJoin(comment).on(comment.todo.id.eq(todo.id))
+                .where(builder)
+                .groupBy(todo.id, todo.title)
+                .orderBy(todo.createdAt.desc())
+                .offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
+
+        JPAQuery<Long> countQuery = jpaQueryFactory
+                .select(todo.count())
+                .from(todo)
+                .where(builder);
+
+        return PageableExecutionUtils.getPage(todoGetResponseList, pageable, countQuery::fetchOne);
     }
 }
