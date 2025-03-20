927abf94b388883ecb1a1cd9cf6030e502e9a89d
queenriwon <queenriwon3@gmail.com>
2025-03-20 22:52:11 +0900

    feat: lv.3-3 S3 연결

---
+++ src/main/java/org/example/expert/domain/common/image/dto/ImageResponse.java
@@ -0,0 +1,10 @@
+package org.example.expert.domain.common.image.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImageResponse {
+    private final String imageUrl;
+}
