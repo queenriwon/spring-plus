958242681a336a97159854ffbd0d4d8e0c0e5084
queenriwon <queenriwon3@gmail.com>
2025-03-21 01:19:41 +0900

    feat: lv.3-3 S3

--- src/main/java/org/example/expert/domain/common/image/controller/ImageController.java
+++ src/main/java/org/example/expert/domain/common/image/controller/ImageController.java
@@ -1,15 +1,10 @@
 package org.example.expert.domain.common.image.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.image.dto.ImageResponse;
-import org.example.expert.domain.common.image.service.S3Service;
+import org.example.expert.domain.common.image.service.S3ImageUploadService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -17,13 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ImageController {
 
-    private final S3Service s3Service;
+    private final S3ImageUploadService imageService;
 
     @PostMapping
     public ResponseEntity<ImageResponse> upload(
-            @RequestPart MultipartFile image
+            @ModelAttribute MultipartFile image
     ) {
-        String imageUrl = s3Service.uploadImage(image);
+        String imageUrl = imageService.uploadImage(image);
         return ResponseEntity.ok(new ImageResponse(imageUrl));
     }
 }
---
+++ src/main/java/org/example/expert/domain/user/dto/request/UserProfileImageRequest.java
@@ -0,0 +1,12 @@
+package org.example.expert.domain.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileImageRequest {
+    private String profileImageUrl;
+}
