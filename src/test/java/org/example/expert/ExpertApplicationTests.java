6c2130c1316f0a561ab4576afbe5718cee5af082
queenriwon <queenriwon3@gmail.com>
2025-03-20 15:56:07 +0900

    feat: lv.3-3 EC2,RDS
    # Conflicts:
    #	.gitignore

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -1,8 +1,11 @@
 package org.example.expert;
 
+import org.example.expert.config.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import({JwtUtil.class})
 @SpringBootTest
 class ExpertApplicationTests {
 
